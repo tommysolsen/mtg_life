af99fccd5a39b0f8afebfb9fc1645b6cdccecc86
Bjørnar Tjosevik <bjornar.tjosevik@lyse.net>
2019-09-29 21:04:33 +0200

    Add backgrounds and icons to guild themes

--- lib/models/life_bar_theme.dart
+++ lib/models/life_bar_theme.dart
@@ -17,12 +17,75 @@ class LifeBarTheme {
         textColor: Colors.white,
         dangerousLifeTotalColor: Colors.red,
       ),
+      "azorius": LifeBarTheme(
+        backgroundColor: Colors.blue,
+        backgroundImage: AssetImage("assets/backgrounds/azorius.jpg"),
+        textColor: Colors.white,
+        dangerousLifeTotalColor: Colors.deepOrange,
+        icon: AssetImage("assets/icons/azorius.jpg")
+      ),
+      "boros": LifeBarTheme(
+          backgroundColor: Colors.red,
+          backgroundImage: AssetImage("assets/backgrounds/boros.jpg"),
+          textColor: Colors.white,
+          dangerousLifeTotalColor: Colors.deepOrange,
+          icon: AssetImage("assets/icons/boros.jpg")
+      ),
+      "dimir": LifeBarTheme(
+          backgroundColor: Colors.blue,
+          backgroundImage: AssetImage("assets/backgrounds/dimir.jpg"),
+          textColor: Colors.white,
+          dangerousLifeTotalColor: Colors.deepOrange,
+          icon: AssetImage("assets/icons/dimir.jpg")
+      ),
+      "golgari": LifeBarTheme(
+          backgroundColor: Colors.green,
+          backgroundImage: AssetImage("assets/backgrounds/golgari.jpg"),
+          textColor: Colors.white,
+          dangerousLifeTotalColor: Colors.deepOrange,
+          icon: AssetImage("assets/icons/golgari.jpg")
+      ),
+      "gruul": LifeBarTheme(
+          backgroundColor: Colors.red,
+          backgroundImage: AssetImage("assets/backgrounds/gruul.jpg"),
+          textColor: Colors.white,
+          dangerousLifeTotalColor: Colors.deepOrange,
+          icon: AssetImage("assets/icons/gruul.jpg")
+      ),
       "izzet": LifeBarTheme(
         backgroundColor: Colors.blue,
         backgroundImage: AssetImage("assets/backgrounds/izzet.jpg"),
         textColor: Colors.white,
         dangerousLifeTotalColor: Colors.red,
         icon: AssetImage("assets/icons/izzet.png")
+      ),
+      "orzhov": LifeBarTheme(
+          backgroundColor: Colors.grey,
+          backgroundImage: AssetImage("assets/backgrounds/orzhov.jpg"),
+          textColor: Colors.white,
+          dangerousLifeTotalColor: Colors.deepOrange,
+          icon: AssetImage("assets/icons/orzhov.jpg")
+      ),
+      "rakdos": LifeBarTheme(
+          backgroundColor: Colors.red,
+          backgroundImage: AssetImage("assets/backgrounds/rakdos.jpg"),
+          textColor: Colors.white,
+          dangerousLifeTotalColor: Colors.deepOrange,
+          icon: AssetImage("assets/icons/rakdos.jpg")
+      ),
+      "selesnya": LifeBarTheme(
+          backgroundColor: Colors.lightGreen,
+          backgroundImage: AssetImage("assets/backgrounds/selesnya.jpg"),
+          textColor: Colors.white,
+          dangerousLifeTotalColor: Colors.deepOrange,
+          icon: AssetImage("assets/icons/selesnya.jpg")
+      ),
+      "simic": LifeBarTheme(
+          backgroundColor: Color(0xFF40e0d0),
+          backgroundImage: AssetImage("assets/backgrounds/simic.jpg"),
+          textColor: Colors.white,
+          dangerousLifeTotalColor: Colors.deepOrange,
+          icon: AssetImage("assets/icons/simic.jpg")
       ),
       "red": LifeBarTheme(
         backgroundColor: Colors.red,
